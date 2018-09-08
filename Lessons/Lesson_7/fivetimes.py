e77c26b559c87c5cef25c087d68554b54b4093c4
Dreamer1cc <dreamer1cc@gmail.com>
2018-09-08 20:48:12 +0300

    while loop against for loop

--- Lessons/Lesson_7/fivetimes.py
+++ Lessons/Lesson_7/fivetimes.py
@@ -1,4 +1,6 @@
-total = 0
-for num in range(101):
-    total += num
-print(total)
+print('My name is ')
+i = 0
+while i < 5:
+# for i in range(5):
+    print('Jimmy Five Times ' + str(i))
+    i += 1
