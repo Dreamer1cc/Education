6c335780b4fc52843810bc0cf495938991346a7d
Kiril Golovchenko <golovchenko@sc.com>
2018-09-10 08:28:52 +0300

    a lot of cats

--- Lessons/Lesson_11/howManyCats.py
+++ Lessons/Lesson_11/howManyCats.py
@@ -1,6 +1,11 @@
 print('How many cats do you have?')
 numCats = input()
-if int(numCats) >= 4:
-    print('That is a lot of cats.')
-else:
-    print('That is not many cats.')
+try:
+    if int(numCats) < 0:
+        print('You cannot have negative count of cats.')
+    elif int(numCats) >= 4:
+        print('That is a lot of cats.')
+    else:
+        print('That is not many cats.')
+except ValueError:
+    print('You did not enter a number.')
